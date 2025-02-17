f5c89903ad30f83fa10ff7e20202bb98b1b3f1ca
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-02-17 15:24:52 -0600

    finished js

---
+++ index.js
@@ -0,0 +1,125 @@
+const dateEl = document.getElementById("date-el");
+const title = document.getElementById("title");
+const input = document.getElementById("input");
+const submitBtn = document.getElementById("submit-btn");
+const notes = document.getElementById("notes");
+
+const itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
+
+
+submitBtn.addEventListener("click", () => {
+    const item = document.querySelector("#input");
+    createItem(item);
+})
+
+
+function createItem(item) {
+    itemsArray.push(item.value);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    location.reload();
+}
+
+function displayItems(){
+    let items = ""
+    for(let i = 0; i < itemsArray.length; i++){
+      items += `<div class="item">
+                  <div class="input-controller">
+                    <textarea disabled>${itemsArray[i]}</textarea>
+                    <div class="edit-controller">
+                      <i class="fa-solid fa-check deleteBtn"></i>
+                      <i class="fa-solid fa-pen-to-square editBtn"></i>
+                    </div>
+                  </div>
+                  <div class="update-controller">
+                    <button class="saveBtn">Save</button>
+                    <button class="cancelBtn">Cancel</button>
+                  </div>
+                </div>`
+    }
+    document.querySelector(".to-do-list").innerHTML = items
+    activateDeleteListeners()
+    activateEditListeners()
+    activateSaveListeners()
+    activateCancelListeners()
+  }
+
+  function activateDeleteListeners(){
+    let deleteBtn = document.querySelectorAll(".deleteBtn")
+    deleteBtn.forEach((dB, i) => {
+      dB.addEventListener("click", () => { deleteItem(i) })
+    })
+  }
+
+  function activateEditListeners(){
+    let editBtn = document.querySelectorAll(".editBtn");
+    const updateController = document.querySelectorAll(".update-controller");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    editBtn.forEach((eb, i) => {
+        eb.addEventListener("click", () => {
+            updateController[i].style.display = "block";
+            inputs[i].disabled = false;
+        })
+    })
+  }
+
+  function activateSaveListeners() {
+    let saveBtn = document.querySelectorAll(".saveBtn");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    saveBtn.forEach((sb, i) => {
+        sb.addEventListener("click", () => {
+            updateItem(inputs[i].value, i);
+        })
+    })
+  }
+
+  function activateCancelListeners() {
+    let cancelBtn = document.querySelectorAll(".cancelBtn");
+    const updateController = document.querySelectorAll(".update-controller");
+    const inputs = document.querySelectorAll(".input-controller textarea");
+    cancelBtn.forEach((cb, i) => {
+        cb.addEventListener("click", () => {
+            updateController[i].style.display = "none";
+            inputs[i].disabled = true;
+        })
+    })
+  }
+
+function deleteItem(i) {
+    itemsArray.splice(i, 1);
+    localStorage.setItem("items", JSON.stringify(itemsArray))
+    location.reload();
+}
+
+function updateItem(text, i) {
+    itemsArray[i] = text;
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    location.reload();
+}
+
+function displayDate() {
+    let date = new Date();
+    let dd = String(date.getDate());
+    let mm = String(date.getMonth() + 1);
+    let yyyy = date.getFullYear();
+    
+    dateEl.textContent = mm + '/' +  dd + '/' + yyyy
+}
+
+
+function addNote() {
+    const key = title.value().trim().replace(" ", "");
+    const text = input.value();
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", key)
+    newDiv.classList.add("note")
+    newDiv.innerHTML += `<p>${title.value()}</p>
+                        <p>${text}</p>
+                            <button class="edit">Edit</button>
+                            <button class="delete">Delete</button>`
+}
+
+window.onload = function() {
+    displayDate();
+    displayItems();
+}
+
